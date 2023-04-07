4705367dc862583e19e1f8ef403035bf8ef02a48
Kiet Nguyen <kietnh@vanthuong.com.vn>
2023-04-07 13:11:19 +0700

    re init with solito + tailwind

--- src/config/app.config.ts
+++
@@ -1,4 +0,0 @@
-export const AppConfig = {
-  apiBase: process.env.NEXT_PUBLIC_API_BASE,
-  enableApiMockup: !!parseInt(process.env.NEXT_PUBLIC_ENABLE_API_MOCKUP || "0"),
-};
--- src/utility/createEmotionCache.ts
+++
@@ -1,7 +0,0 @@
-import createCache from '@emotion/cache';
-
-const createEmotionCache = () => {
-  return createCache({ key: 'mui-style', prepend: true });
-};
-
-export default createEmotionCache;
